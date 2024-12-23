17b6e6e8a5b88081241541b5dc743ecb37a129b9
Devanshukoli <kolidevanshu02@gmail.com>
2024-12-23 17:14:51 +0530

    Add humanChoice and ComputerChoice functions.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Let's goooo!!!")
+let humanScore = 0;
+let computerScore = 0;
+
+// function
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        // return 'rock';
+        console.log('rock')
+    } else if (randomNumber === 1) {
+        // return 'paper';
+        console.log('paper')
+    } else {
+        // return 'scissors';
+        console.log('scissors')
+    }
+}
+
+// function to get human choice.
+function getHumanChoice() {
+    const humanChoice = prompt('Enter your choice: rock, paper or scissors');
+    console.log('human choice: ', humanChoice)
+}
