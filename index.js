d6ede1769180ff824d4ee45c3bab979bc82bba49
Devanshukoli <kolidevanshu02@gmail.com>
2024-12-23 17:58:18 +0530

    Add playRound function

--- index.js
+++ index.js
@@ -21,3 +21,26 @@ function getHumanChoice() {
     const humanChoice = prompt('Enter your choice: rock, paper or scissors');
     console.log('human choice: ', humanChoice)
 }
+
+// function to play the game.
+function playRound(humanChoice, computerChoice) {
+    // Now making this args. case insensitive.
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        return 'It is a tie!';
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        return 'You win!';
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        return 'You win!';
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }   
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
